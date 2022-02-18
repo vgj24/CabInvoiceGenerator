e7fae6309f73e3c31487c40661f8a60b401b4fbf
vgj24 <vrushlijoshi24@gmail.com>
2022-02-18 14:40:35 +0530

    CabInvoiceGenerator

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CabInvoiceGenerator;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Cab Invoice Generator");
+InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+double fare = invoiceGenerator.CalculateFare(2.0, 5);
+InvoiceGenerator invgenrator =new InvoiceGenerator(RideType.PREMIUM);
+double farepremium =invgenrator.CalculateFare(2.0,20);
+Console.WriteLine("fare for normal ride");
+Console.WriteLine($"Fare : {fare}");
+Console.WriteLine("fare for premiumride");
+Console.WriteLine($"Fare : {farepremium}");
+Console.ReadLine();
